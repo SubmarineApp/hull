b31bc7afc8fdecb216a2f42f3153a22a036a2233
Eliot Partridge <byte@code.horse>
2021-02-27 14:02:52 -0600

    Remove hyperlinks from serializers

--- hull/serializers.py
+++ hull/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 
 from .models import Subscription, Category
 
-class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
+class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ('id', 'title', 'category', 'starts_at', 'trial_ends_at', 'ends_at', 'recurrence', 'trial_cost', 'cost')
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
